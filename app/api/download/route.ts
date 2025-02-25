fe4ac469da945df5e25eb7ed27692d70a9975d32
aapelix <mcaapelix@gmail.com>
2025-02-25 15:18:22 +0200

    ...............

--- app/api/download/route.ts
+++ app/api/download/route.ts
@@ -1,10 +1,9 @@
-import ytdl from "@distube/ytdl-core";
 import { NextResponse } from "next/server";
+import ytdl from "@distube/ytdl-core";
 
-export async function GET(request: Request, response: Response) {
+export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const url = searchParams.get("link");
-  const responseHeaders = new Headers(response.headers);
 
   if (!url) {
     return NextResponse.json({ data: "No URL" });
@@ -16,14 +15,14 @@ export async function GET(request: Request, response: Response) {
     const title = videoInfo.videoDetails.title;
     const sanitizedTitle = title.replace(/[^a-zA-Z0-9_-]/g, "_");
 
+    const responseHeaders = new Headers();
     responseHeaders.set(
       "Content-Disposition",
-      `attachment; filename="${sanitizedTitle}.mp4"`,
+      `attachment; filename="${sanitizedTitle}.mp4"`
     );
-
     responseHeaders.set(
       "User-Agent",
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
     );
 
     const format = ytdl.chooseFormat(videoInfo.formats, { quality: 'highestvideo' });
@@ -34,7 +33,7 @@ export async function GET(request: Request, response: Response) {
 
     const data = ytdl(url, { format });
 
-    return new Response(data as any, {
+    return new Response(data, {
       headers: responseHeaders,
     });
   } catch (error) {
